137250dca79f77fa3b260445c308e9ed59bc09cf
João Sombrio <chrisparkerrr@gmail.com>
2020-08-18 22:22:18 +0000

    :heavy_plus_sign: added remix for auto recompile

--- src/cards/mix.exs
+++ src/cards/mix.exs
@@ -14,15 +14,18 @@ defmodule Cards.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      applications: applications(Mix.env)
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:remix, "~> 0.0.1", only: :dev}
     ]
   end
+
+  defp applications(:dev), do: applications(:all) ++ [:remix]
+  defp applications(_all), do: [:logger]
 end
